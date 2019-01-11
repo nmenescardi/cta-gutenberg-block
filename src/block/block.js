446d581e12a9f1227e7a0041df276752060820a7
nmenescardi <nicolasmenescardi@hotmail.com>
2019-01-11 10:08:24 -0300

    Add save and edit methods

--- src/block/block.js
+++ src/block/block.js
@@ -11,7 +11,8 @@ import './editor.scss';
 
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
-const { PlainText } = wp.editor;
+//const { PlainText } = wp.editor;
+const { RichText } = wp.editor;
 
 /**
  * Register: aa Gutenberg Block.
@@ -33,51 +34,40 @@ registerBlockType( 'cgb/block-nm-cta', {
 	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	keywords: [
 		__( 'nm-cta — CGB Block' ),
-		__( 'CGB Example' ),
-		__( 'create-guten-block' ),
+		__( '' ),
+		__( '' ),
 	],
-
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	edit: function( props ) {
-		// Creates a <p class='wp-block-cgb-block-nm-cta'></p>.
+	attributes: {
+		message: {
+			type: 'array',
+			source: 'children',
+			selector: '.message-body',
+		}
+	},
+	edit: props => {
+		const { attributes: { message }, className, setAttributes } = props;
+		const onChangeMessage = message => { setAttributes( { message } ) };
 		return (
-			<div className={ props.className }>
-				<PlainText value='Financial independence that you can count on' onChange={ value => console.log(value) } />
-				<a href="#">JOIN US</a>
-			</div>
+			<div className={ className }>
+				<RichText
+					tagName="div"
+					multiline="p"
+					placeholder={ __( 'Financial independence that you can count on', 'nm-cta' ) }
+					  onChange={ onChangeMessage }
+					  value={ message }
+				  />
+				  <a href="#">JOIN US</a>
+			</div>				
 		);
 	},
-
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	save: function( props ) {
+	save: props => {
+		const { attributes: { message } } = props;
 		return (
 			<div>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>nm-cta</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+				<h2>{ __( 'Call to Action', 'nm-cta' ) }</h2>
+				<div class="message-body">
+					{ message }
+				</div>
 			</div>
 		);
 	},
