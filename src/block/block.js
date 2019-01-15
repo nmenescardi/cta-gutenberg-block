5d1b14157c1df194d68872346b8c3fed2c86c711
nmenescardi <nicolasmenescardi@hotmail.com>
2019-01-15 09:38:52 -0300

    Code Refactoring

--- src/block/block.js
+++ src/block/block.js
@@ -36,17 +36,16 @@ const {
  */
 registerBlockType( 'cgb/block-nm-cta', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'Custom CTA' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+	title: __( 'Custom CTA', 'nm-cta' ), // Block title.
+	//icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	icon: {
 		background: 'rgba(254, 243, 224, 0.52)',
 		src: icon,
 	},
 	keywords: [
-		__( 'nm-cta — CGB Block' ),
-		__( '' ),
-		__( '' ),
+		__( 'Custom CTA' ),
+		__( 'CTA' ),
 	],
 	attributes: {
 		ctaTitle: {
@@ -69,7 +68,6 @@ registerBlockType( 'cgb/block-nm-cta', {
 	edit: props => {
 		const { attributes: { ctaTitle, text, url }, className, isSelected, setAttributes } = props;
 		const onChangeCtaTitle = ctaTitle => { setAttributes( { ctaTitle } ) };
-		const onChangeCtaButtonMsg = ctaButtonMsg => { setAttributes( { ctaButtonMsg } ) };
 		return (
 			<div className={ className }>
 				<RichText
@@ -85,11 +83,11 @@ registerBlockType( 'cgb/block-nm-cta', {
 					<Fragment>
 						<TextControl
 							id="example-input-field"
-							label={ __( 'Link Text', 'jsforwpblocks' ) }
+							label={ __( 'Link Text', 'nm-cta' ) }
 							value={ text }
 							onChange={ text => setAttributes( { text } ) }
 						/>
-						<p>{ __( 'Link URL', 'jsforwpblocks' ) }</p>
+						<p>{ __( 'Link URL', 'nm-cta' ) }</p>
 						<form
 							className="blocks-format-toolbar__link-modal-line blocks-format-toolbar__link-modal-line"
 							onSubmit={ event => event.preventDefault() }
@@ -104,7 +102,7 @@ registerBlockType( 'cgb/block-nm-cta', {
 							/>
 							<IconButton
 								icon="editor-break"
-								label={ __( 'Apply', 'jsforwpblocks' ) }
+								label={ __( 'Apply', 'nm-cta' ) }
 								type="submit"
 							/>
 						</form>
@@ -112,11 +110,11 @@ registerBlockType( 'cgb/block-nm-cta', {
 
 					) : (
 
-					<p>
-						<a href={ url }>
-							{ text || __( 'Edit link', 'jsforwpblocks' ) }
-						</a>
-					</p>
+						<p>
+							<a href={ url }>
+								{ text || __( 'Edit link', 'nm-cta' ) }
+							</a>
+						</p>
 
 					)}
 			</div>				
@@ -126,9 +124,7 @@ registerBlockType( 'cgb/block-nm-cta', {
 		const { attributes: { ctaTitle, text, url } } = props;
 		return (
 			<div>
-				<div class="ctaTitle-body">
-					{ ctaTitle }
-				</div>
+				<div class="ctaTitle-body">{ ctaTitle }</div>
 				<div class="cta-link"><div class="link-wrapper"><a href={ url }>{ text }</a></div></div>
 			</div>
 		);
